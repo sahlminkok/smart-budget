f8642d9d8fc686ed1e43b15e0fe9a39aa858d852
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 10:07:32 +0100

    Fix ruby linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, presence: true, length: {in: 3..25}
+  validates :name, presence: true, length: { in: 3..25 }
 end
