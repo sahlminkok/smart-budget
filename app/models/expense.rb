ac69fbe1311bc7254f3ce2e8516075ef55b222f6
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 11:49:01 +0100

    Add validations to expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,7 @@ class Expense < ApplicationRecord
 
   has_many :group_expenses
   has_many :groups, through: :group_expenses
+
+  validates :name, presence: true, length: { in: 3..25 }
+  validates :amount, numericatlity: { greater_than_or_equal_to: 0 }
 end
