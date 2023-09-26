765dc299febc90e0500ca17f0aeb897d26f0e3ce
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 17:05:22 +0100

    Add tests cases to user_spec model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  before do
+    allow_any_instance_of(Devise::Mailer).to receive(:confirmation_instructions).and_return(double(deliver: true))
+  end
+
+  it 'name should be present' do
+    expect(user.name).to eq('user')
+  end
+
+  it 'email should be present' do
+    expect(user.email).to eq('example@example.com')
+  end
+
+  it 'password should be present' do
+    expect(user.password).to eq('password')
+  end
 end
