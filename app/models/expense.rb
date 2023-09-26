6655dfaa9dacf4ec005685e2b7dd19ff56d12b40
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 12:40:10 +0100

    Fix typos in expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,9 @@
 class Expense < ApplicationRecord
-  belongs_to :author_id, class_name: 'User', foreign_key: author_id
+  belongs_to :author_id, class_name: 'User', foreign_key: 'author_id'
 
   has_many :group_expenses
   has_many :groups, through: :group_expenses
 
   validates :name, presence: true, length: { in: 3..25 }
-  validates :amount, numericatlity: { greater_than_or_equal_to: 0 }
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
