404f9fcce4b051d018e995754dd502443d669783
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 16:54:31 +0100

    Add test cases to group_spec model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  before do
+    allow_any_instance_of(Devise::Mailer).to receive(:confirmation_instructions).and_return(double(deliver: true))
+    @group = Group.create(name: 'group', icon: 'icon', author_id: user.id)
+  end
+
+  it 'name should be present' do
+    @group.name = nil
+    expect(@group).to_not be_valid
+  end
+
+  it 'name length should be > 3 and < 25 chars' do
+    @group.name = 'sa'
+    expect(@group).to_not be_valid
+  end
 end
