b0473123710a4f3cbe5bb8ceb4d3fa26c9ab9c29
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 11:24:35 +0100

    Add an association between the author and expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,3 @@
 class Expense < ApplicationRecord
+  belongs_to :author_id, class_name: 'User', foreign_key: author_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :groups, foreign_key: 'author_id'
+  has_many :expenses, foreign_key: 'author_id'
 
   validates :name, presence: true, length: { in: 3..25 }
 end
