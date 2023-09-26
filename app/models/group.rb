f2f11f70d80c56763f72b7c0a85a1d843e389e22
sahlminkok <sahlminkok7@gmail.com>
2023-09-26 11:43:36 +0100

    Add an association between group and group_expense models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_many :group_expenses
+  has_many :expenses, through: :group_expenses
 end
