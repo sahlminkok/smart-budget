d15eac8a67eabefdc015954a70a1fbecccf05591
sahlminkok <sahlminkok7@gmail.com>
2023-09-27 15:17:15 +0100

    Add a create action in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,22 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = current_user.groups.build(group_params)
+    @group.author = current_user
+
+    if @group.save
+      redirect_to groups_path, notice: 'Category was successfully created'
+    else
+      flash.now[:alert] = 'Category could not be created'
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
