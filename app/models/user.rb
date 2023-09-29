b83d1df6828d28f20fe1ada95aa66daa2d3d33b5
sahlminkok <sahlminkok7@gmail.com>
2023-09-29 09:55:12 +0100

    Remove confirmable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :groups, foreign_key: 'author_id'
   has_many :expenses, foreign_key: 'author_id'
